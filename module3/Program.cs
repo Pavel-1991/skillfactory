0c7c84a95af26b3c387c35e63ee6c2037ecb0baa
Pavel-1991 <ryabikin-pavel@mail.ru>
2023-04-14 20:04:07 +0300

    unit3.9

--- module3/Program.cs
+++ module3/Program.cs
@@ -8,10 +8,11 @@
 
             Console.WriteLine(MyName);
 
-            Console.WriteLine("\t  Привет, мир");
-            Console.WriteLine("\t  Мне 27 лет");
-            Console.WriteLine("\t  My name is \n  Jane");
-
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
             Console.ReadKey();
         }
     }
