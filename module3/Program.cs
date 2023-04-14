6fcf5d191d875246afd6627706115268d30d9dfd
Pavel-1991 <ryabikin-pavel@mail.ru>
2023-04-14 21:15:16 +0300

    unit 3.5.3

--- module3/Program.cs
+++ module3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace module3
+﻿using System;
+
+namespace module3
 {
     internal class Program
     {
@@ -6,6 +8,7 @@
         {
             string MyName = "Jane";
             byte MyAge = 27;
+            byte age = 32;
             bool HaveIApet = true;
             double MyShoeSize = 37.5;
 
@@ -13,6 +16,29 @@
             Console.WriteLine("MyAge " + MyAge);
             Console.WriteLine("Do I have a pet? " + HaveIApet);
             Console.WriteLine("My shoe size is " + MyShoeSize);
+            Console.WriteLine("\tПривет,\n мир!");
+            Console.WriteLine($"Мой возраст {age}");
+
+            DaysOfWeek MyFavoriteDay;
+            MyFavoriteDay = DaysOfWeek.Friday;
+            Console.WriteLine(MyFavoriteDay);
+            
+
+
+        }
+
+        enum DaysOfWeek : byte
+        {
+            Tuesday,
+            Monday,
+            Wednesday,
+            Friday
+        }
+        enum Semaphhore : uint
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
         }
     }
 }
