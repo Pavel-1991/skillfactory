2e5586d9088a1914167a500f4fb52bbd52ced31e
Pavel-1991 <ryabikin-pavel@mail.ru>
2023-04-16 14:45:18 +0300

    unit3.7.3

--- module3/Program.cs
+++ module3/Program.cs
@@ -7,15 +7,27 @@ namespace module3
     {
         static void Main(string[] args)
         {
-            string name = "Jane";
-            byte age = 27;
+            string nameold = "Jane";
+            byte ageold = 27;
             string favcolor = "black";
 
-            Console.WriteLine("{0}\n {1}\n{2}", name, age, favcolor);
+            Console.WriteLine("{0}\n {1}\n{2}", nameold, ageold, favcolor);
+
+            int counter = 10;
+            counter += 15;
+            Console.WriteLine("Value: {0} Increment: {1}", counter, counter);
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.ReadKey();
 
             string str = Console.ReadLine();
 
         }
+
         enum Semaphhore : uint
         {
             Red = 100,
