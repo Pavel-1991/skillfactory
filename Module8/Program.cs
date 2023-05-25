affb6ffc7a8ebf7e11a9e4040476330a39ff6d3d
Pavel-1991 <ryabikin-pavel@mail.ru>
2023-05-25 23:21:19 +0300

    Задание 2

--- Module8/Program.cs
+++ Module8/Program.cs
@@ -6,8 +6,20 @@ namespace Module8
     {
         static void Main(string[] args)
         {
+            
+            try
+            {
+                string FolderdirName = @"C:\Users\Zver\Desktop\SkilF\";
+                DirectoryInfo folder = new DirectoryInfo(FolderdirName);
+                long WeightCatalog = GetWeightCatal(folder);        //Причёсанный метод для задания 2
+                Console.WriteLine($"Размер каталога в байтах = {WeightCatalog}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
-            GetCatalClean();
+            GetCatalClean();        //Задание 1
         }
         static void GetCatalClean()
         {
@@ -57,8 +69,27 @@ namespace Module8
             {
                 Console.WriteLine(e.Message);
             }
+        }
 
-        
+        static long GetWeightCatal(DirectoryInfo folder)
+        {
+
+            long totalSizeDir = 0;
+            FileInfo[] allFiles = folder.GetFiles();
+  
+            foreach (FileInfo file in allFiles)
+            {
+                totalSizeDir += file.Length;
+            }
+            DirectoryInfo[] subFolders = folder.GetDirectories();
+
+            foreach (DirectoryInfo dir in subFolders)
+            {
+                totalSizeDir += GetWeightCatal(dir);
+            }
+
+            return totalSizeDir;
+
         }
     }
 }
