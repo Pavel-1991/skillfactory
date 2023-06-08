55dc6959d6685308e6f1dd46b40f205bdb45407f
Pavel-1991 <ryabikin-pavel@mail.ru>
2023-06-08 20:25:33 +0300

    Итоговое задание

---
+++ Module10/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Module10
+{
+    internal class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main(string[] args)
+        {
+            Logger = new Logger();
+            var Worker1 = new Worker1(Logger);
+            while (true)
+            {
+                try
+                {
+                    Worker1.CalcSum();
+                }
+                catch (Exception ex)
+                {
+                    Logger.Error("Работа алгоритма приостановлена");
+                    Logger.Error($"Произошло следующее исключение: {ex.Message}");
+                    Logger.Error("Начинаем с начала");
+                }
+            }
+        }
+    }
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string message);
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    public interface IWorker
+    {
+        void CalcSum();
+    }
+
+}
---
+++ Module10/Worker1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module10
+{
+    public class Worker1 : IWorker
+    {
+        ILogger Logger { get; }
+        public Worker1(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void CalcSum()
+        {
+            Console.WriteLine("Введите первое число");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Logger.Event($"Введено первое число = {number1}");
+            Console.WriteLine("Введите второе число");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            Logger.Event($"Введено второе число = {number2}");
+            double sumnumbers = number1+ number2;
+            Logger.Event($"Посчитана сумма двух чисел = {sumnumbers}");
+            Logger.Event("Всё отработало успешно, попробуем ещё");
+
+        }
+    }
+}
