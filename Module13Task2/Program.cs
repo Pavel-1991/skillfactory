edfcf80b477c6c85f3c059770b9775420023cc7a
Pavel-1991 <ryabikin-pavel@mail.ru>
2023-07-06 20:05:21 +0300

    task 13.6.2

---
+++ Module13Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Module13Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader Readtxt = new StreamReader("C:\\Users\\Zver\\Desktop\\SkilF\\Text1.txt");
+                string TxtAll = Readtxt.ReadToEnd();
+                TxtAll = TxtAll.Replace("\n", " ");
+                TxtAll = TxtAll.Replace("  ", " ");
+                TxtAll = TxtAll.Trim();
+                TxtAll = new string(TxtAll.Where(c => !char.IsPunctuation(c)).ToArray());
+                
+                string[] ArString = TxtAll.Split(new char[] {' '});
+                Dictionary<string, int> dr = new Dictionary<string, int>();
+                //Размещаем слова в словаре и подсчитываем частотность
+                foreach (string s in ArString)
+                {
+                    if (s.Length > 0)
+                        if (dr.Keys.Contains(s))
+                        {
+                            dr[s]++;
+                        }
+                        else
+                        {
+                            dr.Add(s, 1);
+                        }                          
+                }
+
+                string S = ""; int k = 0;
+                foreach (KeyValuePair<string, int> kk in dr.OrderByDescending(x => x.Value))
+                {
+                    S += "Слово << " + kk.Key + " >> встречается " + kk.Value.ToString() + " раз\n";
+                    k++;
+                    if (k == 10) break;
+                }
+                Console.WriteLine(S);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
