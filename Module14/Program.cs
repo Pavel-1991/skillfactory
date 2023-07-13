81521756c2fae1bdf613d8316ee344b364fdfdd0
Pavel-1991 <ryabikin-pavel@mail.ru>
2023-07-13 15:21:43 +0300

    task

---
+++ Module14/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Module14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortedphoneBook = phoneBook
+            .OrderBy(p => p.Name)
+            .ThenBy(p => p.LastName);
+
+            foreach (var pBook in sortedphoneBook)
+                Console.WriteLine(pBook.Name + ", " + pBook.LastName + ", " + pBook.PhoneNumber + ", " + pBook.Email);
+        }
+    }
+    public class Contact // модель класса
+    {
+        public Contact(string name, string lastName, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+        }
+
+        public String Name { get; }
+        public String LastName { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
