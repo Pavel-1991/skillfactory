1a8f3412b2ff670e3ba479c0b38ed2a206d54828
Pavel-1991 <ryabikin-pavel@mail.ru>
2023-08-17 22:27:28 +0300

    итоговый проект

---
+++ SocialNetwork/BLL/Models/UserAddFriends.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.BLL.Models
+{
+    public class UserAddFriends
+    {
+        public int User_id { get; set; }
+        public string Friend_Email { get; set; }
+    }
+}
---
+++ SocialNetwork/PLL/Views/AddFriendsView.cs
@@ -0,0 +1,48 @@
+﻿using SocialNetwork.BLL.Exceptions;
+using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using SocialNetwork.PLL.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL.Views
+{
+    public class AddFriendsView
+    {
+        UserService userService;
+        public AddFriendsView(UserService userService)
+        {
+            this.userService = userService;
+        }
+        public void Show(User user)
+        {
+            try
+            {
+                var userAddFriends = new UserAddFriends();
+
+                Console.WriteLine("Введите почтовый адрес пользователя которого хотите добавить в друзья: ");
+
+                userAddFriends.Friend_Email = Console.ReadLine();
+                userAddFriends.User_id = user.Id;
+
+                this.userService.AddFriend(userAddFriends);
+
+                SuccessMessage.Show("Вы успешно добавили пользователя в друзья!");
+            }
+
+            catch (UserNotFoundException)
+            {
+                AlertMessage.Show("Пользователя с указанным почтовым адресом не существует!");
+            }
+
+            catch (Exception)
+            {
+                AlertMessage.Show("Произоша ошибка при добавлении пользотваеля в друзья!");
+            }
+
+        }
+    }
+}
